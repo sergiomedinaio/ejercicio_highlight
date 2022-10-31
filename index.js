07d199dcaec2f66d2c4d8a535430205e95d32de6
Sergio Medina <hola@sergiomedina.io>
2022-10-31 19:22:54 -0300

    feat: add product list

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log("Ejemplo de highlight")
-const listaDeProducto = []
+
+const generateProducts = () => {
+    const products = [];
+    products.push({name: "Yerba Mañanita", price: 600.0});
+    products.push({name: "Pimiento Rojo", price: 499.0});
+    products.push({name: "Sandia", price: 169.0});
+    products.push({name: "Zanahoria", price: 129.0});
+    return products;
+}
+
+
+const listOfProduct = generateProducts();
